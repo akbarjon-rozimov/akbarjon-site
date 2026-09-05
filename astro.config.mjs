2b729cfd48f7291afa8b9c2a14f7f064b44b905b
Akbarjon Rozimov <akbarjonruzimov@gmail.com>
2026-09-06 01:34:41 +0800

    Redesign: deep-ocean theme with helix motif

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build
 export default defineConfig({
   site: 'https://akbarjon-rozimov.com',
+  integrations: [sitemap()],
 });
